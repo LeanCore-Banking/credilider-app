a163ebea86a4025300ddb6ef6cd472683defcb62
BlueGroup <bluegroup@192.168.1.4>
2024-10-05 23:38:56 -0500

    responsive home

--- credilider/src/components/HomePage/Index.tsx
+++ credilider/src/components/HomePage/Index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import "./styles.css"
 import Header from "../Header/Index"
 import Banner from "../../../public/banner-home.png"
+import { robotoCondensed } from "@/app/fonts/fonts"
 
 
 const HomePage: React.FC = () => {
@@ -13,12 +14,12 @@ const HomePage: React.FC = () => {
     <div className="layout">
       <Header />
       <main className="main-content">
-        <div className="container">
+        <div className={`${robotoCondensed.className} container`}>
           <div className="content">
             <div className="text-content">
               <h1 className="main-title">¿MOTO NUEVA?</h1>
               <h1 className="main-title"> CREDILIDER TE LA FINANCIA </h1>
-              <strong className="highlight-text">Te financiamos hasta el 100% de tu moto</strong>
+              <h3 className="highlight-text" id="text-contet-sub-title">Te financiamos hasta el 100% de tu moto</h3>
               <p className="disclaimer">*Sujeto a aprobación y estudio de crédito de la entidad que financia.
                 Entidad que financia Socolfi S.A.S
               </p>
@@ -28,8 +29,8 @@ const HomePage: React.FC = () => {
               <Image
                 src={Banner}
                 alt="Moto banner"
-                width={400}
-                height={300}
+                width={500}
+                height={400}
                 className="image-rounded"
               />
             </div>
