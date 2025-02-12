53b25792b1bd8ec473e56d468ecfe56c3a624376
JoseLeancore <jose@leancore.co>
2025-02-12 15:22:43 -0500

    update name enviroment variables

--- src/auth/AuthProvider/aws-exports.ts
+++ src/auth/AuthProvider/aws-exports.ts
@@ -3,12 +3,12 @@
 export async function getAwsConfig() {
 
   return {
-    aws_project_region: process.env.AUHT_AWS_REGION,
+    aws_project_region: process.env.AUTH_AWS_REGION,
     aws_user_pools_id: process.env.AUTH_AWS_USER_POOL_ID,
-    aws_user_pools_web_client_id: process.env.AUHT_AWS_USER_POOL_WEB_CLIENT_ID,
+    aws_user_pools_web_client_id: process.env.AUTH_AWS_USER_POOL_WEB_CLIENT_ID,
     Auth: {
       Cognito: {
-        userPoolClientId: process.env.AUHT_AWS_USER_POOL_WEB_CLIENT_ID,
+        userPoolClientId: process.env.AUTH_AWS_USER_POOL_WEB_CLIENT_ID,
         userPoolId: process.env.AUTH_AWS_USER_POOL_ID,
       },
     }
